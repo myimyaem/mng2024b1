24cb03c99e4240b0042c51c52f559dc028d9b059
myimyaem <137817981+myimyaem@users.noreply.github.com>
2024-04-26 08:32:15 +0000

    task 1

---
+++ mng/tvi/java/Task/Task1.java
@@ -0,0 +1,10 @@
+package mng.tvi.java.Task;
+
+
+class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+
+mng/tvi/java/Task/Task1.java
