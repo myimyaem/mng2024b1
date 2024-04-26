cbf87701775c1783ff887fabda6a6616d17f8f55
myimyaem <137817981+myimyaem@users.noreply.github.com>
2024-04-26 08:50:15 +0000

    task3

---
+++ mng/tvi/java/Task/Task3.java
@@ -0,0 +1,21 @@
+package mng.tvi.java.Task;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        for(int i = 1; i<=check_number;i++){
+            message = (i%2==0) ? i+" is even number" : i+" is odd number";
+            System.out.println(message);
+        }
+
+    }
+}
+
+
+
+// Create an int variable and name it check_number with an initial value of 10.
+// Create a String variable and name it message.
+// Use check_number as a condition on how many loops the program can print.
+// Create a loop that will check and print each number if odd or even numbers using this syntax:  message = (i%2==0) ? i+" is even number" : i+" is odd number";
+// The next line should print message value.
